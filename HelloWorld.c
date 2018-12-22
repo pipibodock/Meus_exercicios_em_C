714181a527a9c065eb056b259f46d8b085d6d79b
Luiz Fellipe <fellipe.mirandamatos@gmail.com>
2018-12-22 15:32:29 -0200

    Primeiros exercícios em C
    - Hello World;
    - Soma de dois inteiros;

---
+++ HelloWorld.c
@@ -0,0 +1,8 @@
+/* include é o import de bibliotecas do C */
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ soma.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int A, B, Z;
+    printf("Insira dois valores para serem somados\n");
+    scanf("%d", &A);
+    scanf("%d", &B);
+    Z = A + B;
+    printf("A soma de A e B é: %d\n", Z);
+}
